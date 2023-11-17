255ed880b361359fabdeeab2457d77f01cd5e71c
maheshwari1026 <146749774+maheshwari1026@users.noreply.github.com>
2023-11-17 23:11:18 +0530

    m

--- src/sample/Student.java
+++ src/sample/Student.java
@@ -3,9 +3,10 @@ package sample;
 public class Student {
 	public static void main(String[] args) {
 		System.out.println("1");
-		
 		System.out.println("2");
 		System.out.println("3");
+		System.out.println(" person2 : 4");
+		System.out.println("5");
 		
 		
 	}
